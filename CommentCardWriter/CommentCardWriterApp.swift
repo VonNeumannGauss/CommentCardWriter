ad495a7cc39cdde0038f09237d5f22aea9ed5f0f
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-03-15 15:20:28 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Marinov, Zachary (Coll) on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
