5a1693d95cdd859711bfb98cdd58f261d1f611ab
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-03-16 15:09:36 +0000

    Produced basic outputView, with an editable TextEditor field, comment area, and toggle to assess user satisfaction

---
+++ CommentCardWriter/View/OutputView.swift
@@ -0,0 +1,50 @@
+//
+//  OutputView.swift
+//  CommentCardWriter
+//
+//  Created by Marinov, Zachary (Coll) on 16/03/2021.
+//
+
+import SwiftUI
+
+struct OutputView: View {
+    
+    @State var textInput: String = "Otherwise, insert a new comment here"
+    @State var toggleHappyWithGeneratedContent: Bool = true
+    
+    var body: some View {
+        
+        let textInputBinding = Binding<String> (
+            get: { self.textInput },
+            set: { newValue in
+                self.textInput = newValue
+            }
+        )
+        
+        NavigationView {
+            VStack {
+                VStack {
+                    Text("Teacher: DPC")
+                    Text("Div: vCX-1")
+                }
+                Form {
+                    Section(header: Text("Generated comment")) {
+                        Text("I am enjoying CS and loving the hands-on, engaging approach to teaching and learning!")
+                            .padding()
+                        Toggle("Happy with the generated content?", isOn: $toggleHappyWithGeneratedContent)
+                            .padding()
+                    }
+                    Section(header: Text("Edit")) {
+                        TextEditor(text: textInputBinding)
+                    }
+                }
+            }.navigationTitle("Output")
+        }
+    }
+}
+
+struct OutputView_Previews: PreviewProvider {
+    static var previews: some View {
+        OutputView()
+    }
+}
